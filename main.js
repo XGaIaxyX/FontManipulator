f7c925a74bd66e50102e50f9c31ebb9bb3a4861e
-Midnight- <104607301+XGaIaxyX@users.noreply.github.com>
2023-06-17 12:35:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(550, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('Pose', gotPoses);
+}
+function draw()
+{
+    background('#969A97')
+}
+function modelLoaded()
+{
+    console.log('Posenet is initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
